a2da683606dc656c2c37cf5f8a9145d14ac5ce5c
Minh Nguyễn <87883380+tonminhce@users.noreply.github.com>
2025-11-21 16:45:51 +0700

    update

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
